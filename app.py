e944ececc99c40e6540858d6394ff8d7c0188021
Nikhil Lamba <46343406+NikhilLamba24@users.noreply.github.com>
2024-08-05 14:10:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ async def index():
     return FileResponse("index.html")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.2", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
